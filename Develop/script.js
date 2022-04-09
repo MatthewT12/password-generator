cb38e4b4d531d64e49bb5ac2bb156c9b4835a984
MatthewT12 <matttyndell@gmail.com>
2022-04-09 14:53:11 -0400

    created password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,49 @@
 // Assignment code here
 function generatePassword() {
+  //research how to use confirm in javascript
+  //TODO: create prompts for password criteria
+  //create variables 
+  //TODO: create a prompt for password length --> 8 - 128 
+  //TODO: create different password criteria --> uppercase, lowercase, etc.
+  //TODO: randomize the characters to generate the password
+  //TODO: return password for use in other functions
+
+  var passwordSize = window.prompt("Enter size of password (8 - 128 characters)");
+  minPasswordLength = 8;
+  maxPasswordLength = 128;
+
+  console.log(passwordSize)
+
+  var isLowercase = window.confirm("Do you want your password to include lowercase?")
+
+  console.log(isLowercase)
+
+  var isUpperCase = window.confirm("Do you want your password to include uppercase?")
+
+  console.log(isUpperCase)
+
+  var isNumbers = window.confirm("Do you want your password to include numbers?")
+
+  console.log(isNumbers)
+
+  var isSpecialCharacters = window.confirm("Do you want your password to include special characters?")
+
+  console.log(isSpecialCharacters)
+
+  return "fbgberfb";
+
+
+
+  
+  /*if (passwordSize.length >= minPasswordLength && passwordSize.length <= maxPasswordLength) {
+        window.prompt("Do you want your password to include uppercase letters, lowercase letters, numbers, and special characters?");
+        return "sdfjnrfnb";
+      }
+  else {
+        alert("Please input between " +minPasswordLength+ " and " +maxPasswordLength+ " characters");
+        return null;
+      }
+  */
   
 }
 
